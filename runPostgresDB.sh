4d32bcd30e2ab2ff9787e5896ea35a8b6cc6813b
kzyskowski <karol.zyskowski@lppsa.com>
2021-11-01 17:55:54 +0100

    Add postgres to project.

---
+++ runPostgresDB.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$(docker network ls | grep -c 'postgres-local')" -le 0 ];
+then
+  echo 'Docker network postgres-local does not exist. Start create postgres docker network'
+  echo 'Start create postgres docker network'
+  docker network create postgres-local
+else
+  echo 'Docker network postgres-local exists'
+fi
+
+docker rmi "$(docker images -f "dangling=true" -q)" 2> /dev/null || true
+docker stop some-postgres 2> /dev/null || true
+docker rm some-postgres 2> /dev/null || true
+
+docker pull postgres
+docker run -d -p 5432:5432 --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
